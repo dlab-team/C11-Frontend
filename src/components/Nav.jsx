0a04d3e6239c5676d0c0f160bc8e9adecd80b391
name <stb539@gmail.com>
2023-08-14 12:00:02 -0400

    se desarrolla menú del navbar en vista celular

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,26 @@
 import { Disclosure } from "@headlessui/react"
-import { X, Menu  } from 'lucide-react'
+import { X, Menu, Home, UserCheck } from "lucide-react"
 import logo_blanco from "../assets/logo_blanco.png"
+import logo_azul from "../assets/logo_azul.png"
+import { useState } from "react"
 
 const navigation = [
-  { name: "Inicio", href: "/", current: true },
-  { name: "Login", href: "/login", current: false },
-  { name: "Registro", href: "/registro", current: false },
-  { name: "Contacto", href: "/contacto", current: false },
+  {
+    name: "Inicio",
+    href: "/",
+    current: true,
+    icono: <Home className="iconBlue icon_cel h-8 w-8" aria-hidden="true" />,
+  },
+  {
+    name: "Iniciar Sesión",
+    href: "/login",
+    current: false,
+    icono: (
+      <UserCheck className="iconBlue icon_cel h-8 w-8" aria-hidden="true" />
+    ),
+  },
+  /* { name: "Registro", href: "/registro", current: false },
+  { name: "Contacto", href: "/contacto", current: false }, */
 ]
 
 function classNames(...classes) {
@@ -14,18 +28,16 @@ function classNames(...classes) {
 }
 
 const Nav = () => {
+  const [navColor, setNavColor] = useState("bg-[#2738F5]")
+  const [logo, setLogo] = useState(logo_blanco)
   return (
     <div className="min-h-full">
-      <Disclosure as="nav" className="bg-[#2738F5]">
+      <Disclosure as="nav" className={navColor}>
         {({ open }) => (
           <>
-            <div className="nav px-4 sm:px-6 lg:px-8 flex justify-between">
+            <div className="nav mb-4 px-4 sm:px-6 lg:px-8 flex justify-between">
               <div className="flex-none ">
-                <img
-                  className="logo_blanco"
-                  src={logo_blanco}
-                  alt="Your Company"
-                />
+                <img className="logo_blanco" src={logo} alt="Your Company" />
               </div>
               <div className="hidden md:block">
                 <div className="mt-4 p-4 ml-10 flex items-baseline space-x-4 ">
@@ -36,43 +48,52 @@ const Nav = () => {
                   </span>
                 </div>
               </div>
-              <Disclosure.Panel className="sm:hidden">
-                <div className="space-y-1 pb-3 pt-0">
-                  {navigation.map((item) => (
-                    <Disclosure.Button
-                      key={item.name}
-                      as="a"
-                      href={item.href}
-                      className={classNames(
-                        item.current
-                          ? "bg-white-900 text-dark"
-                          : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                        "block rounded-md px-3 py-2 text-base font-medium"
-                      )}
-                      aria-current={item.current ? "page" : undefined}
-                    >
-                      {item.name}
-                    </Disclosure.Button>
-                  ))}
-                </div>
-              </Disclosure.Panel>
               <div className="mt-2 md:hidden">
                 {/* Mobile menu button */}
-                <Disclosure.Button className="mt-4 relative inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-blue-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                <Disclosure.Button
+                  className={classNames(
+                    open
+                      ? "mt-4 relative inline-flex items-center justify-center rounded-md p-2 "
+                      : "mt-4 relative inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-blue-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                  )}
+                >
                   <span className="absolute -inset-0.5" />
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <>
+                      {setNavColor("bg-white")}
+                      {setLogo(logo_azul)}
                       <X className="block h-6 w-6" aria-hidden="true" />
                     </>
                   ) : (
                     <>
+                      {setNavColor("bg-[#2738F5]")}
+                      {setLogo(logo_blanco)}
                       <Menu className="block h-6 w-6" aria-hidden="true" />
                     </>
                   )}
                 </Disclosure.Button>
               </div>
             </div>
+            <Disclosure.Panel className="h-screen sm:hidden">
+              <div className="space-y-1 pb-3 pt-0">
+                {navigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className={classNames(
+                      item.current ? "text-dark" : "font-normal",
+                      "flex flex-row rounded-md px-3 py-2 text-base font-medium"
+                    )}
+                    aria-current={item.current ? "page" : undefined}
+                  >
+                    {item.icono}
+                    <p className="text-2xl">{item.name}</p>
+                  </Disclosure.Button>
+                ))}
+              </div>
+            </Disclosure.Panel>
           </>
         )}
       </Disclosure>
