1d6f04999ebba9f850c19e7366b381c66dc4b7d1
name <stb539@gmail.com>
2023-08-15 13:10:59 -0400

    creación del header (título y párrafo)

--- src/pages/landing/ProgramExplanation.jsx
+++ src/pages/landing/ProgramExplanation.jsx
@@ -1,7 +1,24 @@
-
 const ProgramExplanation = () => {
   return (
-    <div>ProgramExplanation</div>
+    <header>
+      <div className="fondo_celeste">
+      <h1 className="pt-10 sm:text-base text-center lg:text-3xl font-semibold text-[#140B34]">
+          Sin Juniors no hay Seniors
+        </h1>
+        <p className="mt-10 text-center lg:text-2xl text-[#232323]">
+          En este programa unimos la{" "}
+          <span className="texto_azul">
+            rapidez, eficiencia y visión de un desarrollador junior
+          </span>{" "}
+          junto con la{" "}
+          <span className="texto_azul">
+            experiencia y liderazgo de un senior TI
+          </span>
+          , quien acompañará al talento junior contratado para potenciar tu área
+          digital, acá te explicamos cómo:
+        </p>
+      </div>
+    </header>
   )
 }
 
