56e83f7f3492e2c5dfd5146caff97828a9b960b4
name <stb539@gmail.com>
2023-08-16 12:48:49 -0400

    se almacenan imágenes dentro de carpeta landing de assets, se crean 3 columnas y se agregan variables al archivo de configuración de tailwind

--- src/pages/landing/ProgramExplanation.jsx
+++ src/pages/landing/ProgramExplanation.jsx
@@ -1,24 +1,68 @@
+import mid1 from "../../assets/landing/mid1.png"
+import mid2 from "../../assets/landing/mid2.png"
+import mid3 from "../../assets/landing/mid3.png"
+
 const ProgramExplanation = () => {
   return (
-    <header>
-      <div className="fondo_celeste">
-      <h1 className="pt-10 sm:text-base text-center lg:text-3xl font-semibold text-[#140B34]">
-          Sin Juniors no hay Seniors
-        </h1>
-        <p className="mt-10 text-center lg:text-2xl text-[#232323]">
-          En este programa unimos la{" "}
-          <span className="texto_azul">
-            rapidez, eficiencia y visión de un desarrollador junior
-          </span>{" "}
-          junto con la{" "}
-          <span className="texto_azul">
-            experiencia y liderazgo de un senior TI
-          </span>
-          , quien acompañará al talento junior contratado para potenciar tu área
-          digital, acá te explicamos cómo:
-        </p>
-      </div>
-    </header>
+    <div>
+      <header>
+        <div className="text-center bg-celeste pb-28 pt-10 px-9">
+          <h1 className="pt-10 sm:text-base text-center lg:text-3xl font-semibold text-[#140B34]">
+            Sin Juniors no hay Seniors
+          </h1>
+          <p className="mt-10 lg:text-2xl text-[#232323]">
+            En este programa unimos la{" "}
+            <span className="texto_azul">
+              rapidez, eficiencia y visión de un desarrollador junior
+            </span>{" "}
+            junto con la{" "}
+            <span className="texto_azul">
+              experiencia y liderazgo de un senior TI
+            </span>
+            , quien acompañará al talento junior contratado para potenciar tu
+            área digital, acá te explicamos cómo:
+          </p>
+        </div>
+      </header>
+      <section>
+        <div className=" px-9 grid lg:grid-cols-3 gap-4 justify-items-center text-center lg:bg-celeste">
+          <article>
+            <div className="pb-20">
+              <img src={mid1} alt="" className="mb-14 max-h-36 inline" />
+              <h1 className="font-semibold">Aceleramos</h1>
+              <p>
+                Aceleramos el proceso de contratación. Una vez que nos envíes tu
+                necesidad de talento digital recibirás un listado con los
+                mejores candidatos en 5 días.
+              </p>
+            </div>
+          </article>
+          <article>
+            <img
+              src={mid2}
+              alt=""
+              className="mb-14 max-h-36 inline
+            "
+            />
+            <h1 className="font-semibold">Acompañamos</h1>
+            <p>
+              Nosotros nos hacemos cargo, tú creces. Todos los desarrolladores
+              que contrates serán guiados y apoyados a nivel técnico por un
+              mentor senior TI experto en tu área sin ningún costo ($0).
+            </p>
+          </article>
+          <article>
+            <img src={mid3} alt="" className="mb-14  max-h-36 inline" />
+            <h1 className="font-semibold">Potenciamos</h1>
+            <p>
+              Potenciamos y retenemos el talento. Realizamos un seguimiento de
+              alta calidad para potenciar el crecimiento y desarrollo técnico de
+              todos los desarrolladores contratados.
+            </p>
+          </article>
+        </div>
+      </section>
+    </div>
   )
 }
 
