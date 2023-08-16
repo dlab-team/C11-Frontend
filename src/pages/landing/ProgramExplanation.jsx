c4f312d818d6bb90d823ad45668d2256c87f0cd3
name <stb539@gmail.com>
2023-08-16 16:27:33 -0400

    se genera el carrusel para vista en celular de las columnas

--- src/pages/landing/ProgramExplanation.jsx
+++ src/pages/landing/ProgramExplanation.jsx
@@ -1,16 +1,17 @@
 import mid1 from "../../assets/landing/mid1.png"
 import mid2 from "../../assets/landing/mid2.png"
 import mid3 from "../../assets/landing/mid3.png"
+import { Carousel } from "@material-tailwind/react"
 
 const ProgramExplanation = () => {
   return (
     <div>
       <header>
-        <div className="text-center bg-celeste pb-28 pt-10 px-9">
-          <h1 className="titulo pt-10 sm:text-base lg:text-3xl text-[#140B34]">
+        <div className="text-center bg-celeste pb-0 pt-0 px-9">
+          <h1 className="titulo pt-3 sm:pt-10 sm:text-base lg:text-3xl text-[#140B34]">
             Sin Juniors no hay Seniors
           </h1>
-          <p className="pt-2 lg:text-2xl text-[#232323]">
+          <p className=" sm:pt-3 text-sm lg:text-2xl text-[#232323]">
             En este programa unimos la{" "}
             <span className="texto_azul">
               rapidez, eficiencia y visión de un desarrollador junior
@@ -24,42 +25,37 @@ const ProgramExplanation = () => {
           </p>
         </div>
       </header>
-      <section>
-        <div className=" px-9 grid lg:grid-cols-3 gap-4 justify-items-center text-center lg:bg-celeste">
-          <article>
-            <div className="pb-20">
-              <img src={mid1} alt="" className="mb-14 max-h-36 inline" />
-              <h1 className="titulo">Aceleramos</h1>
-              <p>
-                Aceleramos el proceso de contratación. Una vez que nos envíes tu
-                necesidad de talento digital recibirás un listado con los
-                mejores candidatos en 5 días.
-              </p>
-            </div>
-          </article>
-          <article>
-            <img
-              src={mid2}
-              alt=""
-              className="mb-14 max-h-36 inline
-            "
-            />
-            <h1 className="titulo">Acompañamos</h1>
+      <section className="bg-celeste pb-28">
+        <div className=" overflow-x-auto mx-4 sm:mx-0 sm:px-9 grid  sm:grid-cols-3 gap-4 justify-items-center text-center ">
+          <Carousel className="rounded-xl">
+            <article className="card max-w-sm overflow-hidden shadow-lg bg-[#FFFFFF] sm:bg-celeste">
+            <img src={mid1} alt="" className="mb-14 max-h-36 inline" />
+            <h1 className="titulo">Aceleramos</h1>
             <p>
-              Nosotros nos hacemos cargo, tú creces. Todos los desarrolladores
-              que contrates serán guiados y apoyados a nivel técnico por un
-              mentor senior TI experto en tu área sin ningún costo ($0).
+              Aceleramos el proceso de contratación. Una vez que nos envíes tu
+              necesidad de talento digital recibirás un listado con los mejores
+              candidatos en 5 días.
             </p>
           </article>
-          <article>
-            <img src={mid3} alt="" className="mb-14  max-h-36 inline" />
-            <h1 className="titulo">Potenciamos</h1>
+            <article className="card max-w-sm overflow-hidden shadow-lg bg-[#FFFFFF] sm:bg-celeste">
+            <img src={mid2} alt="" className="mb-14 max-h-36 inline" />
+            <h1 className="titulo">Aceleramos</h1>
             <p>
-              Potenciamos y retenemos el talento. Realizamos un seguimiento de
-              alta calidad para potenciar el crecimiento y desarrollo técnico de
-              todos los desarrolladores contratados.
+              Aceleramos el proceso de contratación. Una vez que nos envíes tu
+              necesidad de talento digital recibirás un listado con los mejores
+              candidatos en 5 días.
             </p>
           </article>
+            <article className="card max-w-sm overflow-hidden shadow-lg bg-[#FFFFFF] sm:bg-celeste">
+            <img src={mid3} alt="" className="mb-14 max-h-36 inline" />
+            <h1 className="titulo">Aceleramos</h1>
+            <p>
+              Aceleramos el proceso de contratación. Una vez que nos envíes tu
+              necesidad de talento digital recibirás un listado con los mejores
+              candidatos en 5 días.
+            </p>
+          </article>
+          </Carousel>
         </div>
       </section>
     </div>
