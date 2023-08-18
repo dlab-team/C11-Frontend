7bffab8928fa3b9fee3300e22b493b27f7ab4eec
Mauricio <stb539@gmail.com>
2023-08-18 14:07:54 -0400

    se crea carpeta forms, para contener los formularios y dentro de esta se crea componente RegisterForm para el formulario de registro

--- src/pages/access/Register.jsx
+++ src/pages/access/Register.jsx
@@ -1,7 +1,11 @@
+import RegisterForm from "../../components/forms/RegisterForm"
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="mt-44 pt-4  font-semibold">
+      <h1 className=" mb-14 text-center text-5xl">Regístrate</h1>
+      <RegisterForm />
+    </div>
   )
 }
 
