b697c6a6ee3e7e1a063d4aaf721402427d238aff
Mauricio <stb539@gmail.com>
2023-08-19 11:32:24 -0400

    se desarrolla primera parte de login modo celular y escritorio

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import LoginForm from "../components/LoginForm"
+
+const Login = () => {
+  return (
+    <div className="mt-44 pt-4  font-semibold">
+      <h1 className=" mb-14 text-center text-5xl">Iniciar sesión</h1>
+      <LoginForm />
+      
+    </div>
+  )
+}
+
+export default Login
