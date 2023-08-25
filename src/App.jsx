2560af7840d5ad6f36e9c9f12a5367f0814c07e8
javiergp23 <javgon23@hotmail.com>
2023-08-25 20:17:13 -0300

    Landing Main Talento TI

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { React } from 'react';
+import MainBuscasTalento from './components/Landing/MainBuscasTalento.jsx';
+import MainBuscasTrabajo from './components/Landing/MainBuscasTrabajo.jsx';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <MainBuscasTalento
+    titulo='¿Buscas talento TI?'
+    resumen='Te encontramos profesionales en 5 días y los acompañamos por 3 meses con  tutores senior para potenciar y acelerar sus habilidades técnicas.'
+    />
+    {/* <MainBuscasTrabajo
+    titulo='¿Buscas Trabajo TI'
+    resumen='Te contactamos con empresas de chile y el extranjero que buscan profesionales TI.'
+    /> */}
     </>
   )
 }
