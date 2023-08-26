a99ce9be00dfc8787682295166c3613ca7020d17
javier <archubyster@gmail.com>
2023-08-26 17:50:04 -0300

    modelando layout

--- src/components/Landing/Banner.jsx
+++ src/components/Landing/Banner.jsx
@@ -3,10 +3,12 @@
 
 function Banner() {
   return (
-    <div className="w-[1440px] h-[479]">
-      <h1 className="">¿Cómo funcionamos?</h1>
+    <div className="w-[1440px] h-[479] mb-7">
+      <h1 className=" [#140B34] w-96 h-12 mt-28 text-center ml-44 font-bold font-poppins text-2xl font-semibold leading-10">
+        ¿Cómo funcionamos?
+      </h1>
       <div className="flex flex-auto w-50">
-        <p className="text-[#140B34] w-[590px] h-[144px] text-center text-[25px] not-italic font-normal leading-[normal];">
+        <p className="text-[#140B34] w-[590px] h-[144px] text-center text-[25px] ml-24 mr-4 mt-9 not-italic font-normal leading-[normal];">
           Da click a nuestro video y en sólo 60 segundos conoce{" "}
           <span className="text-[#2738F5] text-2xl not-italic font-semibold leading-[normal];">
             Devsafio
