a298c9675b8498c6bdf26cb2c9b54804a2ba6c01
Gonzalo Fleming <gonzafg2@gmail.com>
2023-08-29 22:46:17 -0400

    Dev (#20)
    * MV_se modifica App.jsx
    
    * se crea carpeta components y archivo Nav.jsx en donde se trabajará en el NavBar del proyecto
    
    * se almacena logo_blanco y logo_azul en carpeta assets
    
    * se instala tailwind y 3 dependencias @headlessui, @heroicons y react-router-dom
    
    * se crea carpeta pages que contendrá los archivos de las vistas del sitio, el archivo Home para la vista principal, y la barra de navegación
    
    * se cambia biblioteca de iconos
    
    * se desarrolla menú del navbar en vista celular
    
    * se pinta azul el icono X del menú navbar de la vista en el celular
    
    * se le dá estilo al botón Inicia Sesión
    
    * se agrega link a Home al logo
    
    * se crea carpeta landing que contendrá el Home, el componente para la explicación del programa y otros componentes del landing
    
    * creación del header (título y párrafo)
    
    * se cambia bg-white por hexadecimal
    
    * se almacenan imágenes dentro de carpeta landing de assets, se crean 3 columnas y se agregan variables al archivo de configuración de tailwind
    
    * titulo
    
    * se instala material/tailwind para el carrusel
    
    * se genera el carrusel para vista en celular de las columnas
    
    * feat: structura de formulario
    
    * fix: recover App.css
    
    * se agrega el footer al landing
    
    * feat: construcion de mobile
    
    * feat: estylos para desktop
    
    * feat: mobile and desktop styles
    
    * se crea componente LoginForm donde se trabajará para generar el formulario de logeo
    
    * se desarrolla primera parte de login modo celular y escritorio
    
    * se desarrolla segunda parte de login, se copian iconos de rrss  a carpeta assets y se crea componente Social_login que contiene botones para acceder mediante redes sociales, vistas celular y escritorio
    
    * se agregan constantes al formulario login
    
    * se agregan constante para red social
    
    * fix: margin-buttom agregado, footer esta debajo
    
    * fix: se cambia estilo fuentes de botones de Social_login según figma
    
    * fix: se modifican estilos y agregan vínculos para Registro y Olvido de contraseña
    
    * feat: pixels to rem
    
    * fix: se agrega título para vista desktop y modifican estilos de botones
    
    * se guardan estilos en constante
    
    * feat:se desarrolla linea vertical
    
    * fix: se modifican márgenes de inputs LoginForm
    
    * fix: estilos de botones corregidos
    
    ---------
    
    Co-authored-by: MauricioVera <stb539@gmail.com>
    Co-authored-by: Artem <lokomotivspb@gmail.com>

---
+++ src/components/Nav.jsx
@@ -0,0 +1,112 @@
+import { Link } from "react-router-dom"
+import { Disclosure } from "@headlessui/react"
+import { X, Menu, Home, UserCheck } from "lucide-react"
+import logo_blanco from "../assets/logo_blanco.png"
+import logo_azul from "../assets/logo_azul.png"
+import { useState } from "react"
+
+const ruta_login = "/login"
+
+const navigation = [
+  {
+    name: "Inicio",
+    href: "/",
+    current: true,
+    icono: <Home className="iconBlue icon_cel h-8 w-8" aria-hidden="true" />,
+  },
+  {
+    name: "Iniciar Sesión",
+    href: ruta_login,
+    current: false,
+    icono: (
+      <UserCheck className="iconBlue icon_cel h-8 w-8" aria-hidden="true" />
+    ),
+  },
+  /* { name: "Registro", href: "/registro", current: false },
+  { name: "Contacto", href: "/contacto", current: false }, */
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ")
+}
+
+const Nav = () => {
+  const [navColor, setNavColor] = useState("bg-[#2738F5]")
+  const [logo, setLogo] = useState(logo_blanco)
+  return (
+    <div className="min-h-full">
+      <Disclosure as="nav" className={navColor}>
+        {({ open }) => (
+          <>
+            <div className="nav px-4 sm:px-6 lg:px-8 flex justify-between">
+              <div className="flex-none ">
+                <Link to="/">
+                  <img className="logo_blanco" src={logo} alt="Your Company" />
+                </Link>
+              </div>
+              <div className="hidden md:block">
+                <div className="p-4 ml-10 flex items-baseline space-x-4 ">
+                  <Link to={ruta_login}>
+                    <button className="bg-[#FFFFFF] rounded-lg p-4 font-normal text-xl text-[#2738F5] hover:text-[#FFFFFF] hover:bg-[#2738F5] ">
+                      Inicia Sesión
+                    </button>
+                  </Link>
+                </div>
+              </div>
+              <div className="mt-2 md:hidden">
+                {/* Mobile menu button */}
+                <Disclosure.Button
+                  className={classNames(
+                    open
+                      ? "mt-4 relative inline-flex items-center justify-center rounded-md p-2 "
+                      : "mt-4 relative inline-flex items-center justify-center rounded-md p-2 text-[#FFFFFF] hover:bg-blue-700 hover:text-[#FFFFFF] focus:outline-none focus:ring-2 focus:ring-[#FFFFFF] focus:ring-offset-2 focus:ring-offset-gray-800"
+                  )}
+                >
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <>
+                      {setNavColor("bg-[#FFFFFF]")}
+                      {setLogo(logo_azul)}
+                      <X
+                        className="iconBlue block h-6 w-6"
+                        aria-hidden="true"
+                      />
+                    </>
+                  ) : (
+                    <>
+                      {setNavColor("bg-[#2738F5]")}
+                      {setLogo(logo_blanco)}
+                      <Menu className="block h-6 w-6" aria-hidden="true" />
+                    </>
+                  )}
+                </Disclosure.Button>
+              </div>
+            </div>
+            <Disclosure.Panel className="h-screen sm:hidden">
+              <div className="space-y-1 pb-3 pt-0">
+                {navigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className={classNames(
+                      item.current ? "text-dark" : "font-normal",
+                      "flex flex-row rounded-md px-3 py-2 text-base font-medium"
+                    )}
+                    aria-current={item.current ? "page" : undefined}
+                  >
+                    {item.icono}
+                    <p className="text-2xl">{item.name}</p>
+                  </Disclosure.Button>
+                ))}
+              </div>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
+    </div>
+  )
+}
+
+export default Nav
