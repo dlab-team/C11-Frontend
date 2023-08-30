7bd1c048805e298d969c15cb26146c2dd4c0b13f
MxzGnTh <mixzio.gonzalez@gmail.com>
2023-08-30 10:36:05 -0400

    llamada de cambios no resuelto2

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import Login from "./pages/Login"
 import Home from "./pages/landing/Home";
 import FloatingButton from "./components/FloatingButton";
 import { React } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import "./App.css"
+import Nav from "./components/Nav"
+import Login from "./pages/Login"
+import Home from "./pages/landing/Home"
 
 function App() {
 	return (
@@ -20,6 +25,17 @@ function App() {
 			</Router>
 		</main>
 	);
+  return (
+    <main className="app">
+      <Router>
+        <Nav />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path='/login' element={ <Login /> } />
+        </Routes>
+      </Router>
+    </main>
+  )
 }
 
 export default App;
--- src/pages/landing/Home.jsx
+++ src/pages/landing/Home.jsx
@@ -1,11 +1,13 @@
 import Footer from "../../components/Footer";
 import InitialContact from "./InitialContact";
 import FloatingButton from "../../components/FloatingButton";
+import Footer from "../../components/Footer";
 
 const Home = () => {
 	return (
 		<>
 			<div>
+
 				<FloatingButton />
 				<InitialContact />
 				<Footer />
@@ -14,4 +16,5 @@ const Home = () => {
 	);
 };
 
-export default Home;
+
+export default Home
