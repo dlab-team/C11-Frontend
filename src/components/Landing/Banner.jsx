acb5074d3bc685e63797fca720b28f3da2579444
javier <archubyster@gmail.com>
2023-08-30 15:04:28 -0300

    falta visibilidad mobile

--- src/components/Landing/Banner.jsx
+++ src/components/Landing/Banner.jsx
@@ -3,23 +3,26 @@
 
 function Banner() {
   return (
-    <div className="w-[1440px] h-[479] mb-7">
-      <h1 className=" [#140B34] w-96 h-12 mt-28 text-center ml-44 font-bold font-poppins text-2xl font-semibold leading-10">
+    <div className="w-[90rem] h-[29.9rem] flex flex-col ">
+      <h1 className=" [#140B34] sm:w-[19.6rem] sm:h-[8.1rem] text-center font-bold font-poppins text-[2.5rem] font-semibold ">
         ¿Cómo funcionamos?
       </h1>
-      <div className="flex flex-auto w-50">
-        <p className="text-[#140B34] w-[590px] h-[144px] text-center text-[25px] ml-24 mr-4 mt-9 not-italic font-normal leading-[normal];">
-          Da click a nuestro video y en sólo 60 segundos conoce{" "}
-          <span className="text-[#2738F5] text-2xl not-italic font-semibold leading-[normal];">
-            Devsafio
-          </span>
-          y cómo estamos{" "}
-          <span className="text-[#2738F5] text-2xl not-italic font-semibold leading-[normal];">
-            cambiando el mundo TI
-          </span>
-          , acelerando y potenciando las células tecnológicas.
-        </p>
-        <div className="video-container">
+
+      <div className="w-[75rem] h-[21.25rem]  ">
+        <div className="w [40.25rem]">
+          <p className="text-[#140B34] w-[19.6rem] h-[9rem] text-center text-[1.5rem] ml-24 mr-4 mt-9 not-italic font-normal;">
+            Da click a nuestro video y en sólo 60 segundos conoce{" "}
+            <span className="text-[#2738F5] text-2xl not-italic font-semibold leading-[normal];">
+              Devsafio
+            </span>
+            y cómo estamos{" "}
+            <span className="text-[#2738F5] text-2xl not-italic font-semibold leading-[normal];">
+              cambiando el mundo TI
+            </span>
+            , acelerando y potenciando las células tecnológicas.
+          </p>
+        </div>
+        <div className="video-container w-[34.75rem] h-[21.25rem]  ">
           <iframe
             width="560"
             height="315"
