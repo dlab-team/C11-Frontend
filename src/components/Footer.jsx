77853dc276161c469ef289a74d32bb2fb2880efb
Artem <lokomotivspb@gmail.com>
2023-09-01 18:12:01 -0400

    feat: ocultar Footer en vista móvil

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
 const Footer = () => {
-  return <footer className="h-28  w-full  bg-[#2738F5]"></footer>
-}
+  return (
+    <footer className="h-28  w-full  bg-[#2738F5] hidden lg:block"></footer>
+  );
+};
 
-export default Footer
+export default Footer;
