7493f2594c90dbf26b2c9dfec5d2c5ecbb8ba25c
Mauricio <stb539@gmail.com>
2023-09-26 21:10:25 -0300

    Hu c11 45/mvera (#15)
    * se crea archivo RestorePass para desarrollar vista de restablecimiento de contraseña
    
    * se desarrolla vista mobil y desktop para restablecer contraseña

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css"
 import Nav from "./components/Nav"
 import Login from "./pages/Login"
 import Home from "./pages/landing/Home"
+import RestorePass from "./pages/RestorePass"
 import RecoverPass from "./components/RecoverPass"
 import Register from "./pages/access/Register"
 import Footer from "./components/Footer"
@@ -15,6 +16,7 @@ function App() {
         <Nav />
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/restorePass" element={<RestorePass />} />
           <Route exact path="/recoverPass" element={<RecoverPass />} />
           <Route exact path='/register' element={ <Register /> } />
           <Route exact path='/login' element={ <Login /> } />
