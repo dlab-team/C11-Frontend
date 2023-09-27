98ed18ae86b99a978641e856f0c3c460ec3bdf2e
Artem Liamichev <lokomotivspb@gmail.com>
2023-09-27 15:30:32 -0300

    C11 41/aliamichev (#24)
    * feat: structure for mobile
    
    * feat: vista mobile
    
    * feat: ocultar Footer en vista móvil
    
    * feat: ocultar Introduction en vista desktop
    
    * feat: vista desktop
    
    * fix: se ha agregado padding para seccion
    
    * fix: button background white

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 // import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import "./App.css"
-import Nav from "./components/Nav"
-import Login from "./pages/Login"
-import Home from "./pages/landing/Home"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Nav from "./components/Nav";
+import Login from "./pages/Login";
+import Home from "./pages/landing/Home";
 import RestorePass from "./pages/RestorePass"
 import RecoverPass from "./components/RecoverPass"
-import Register from "./pages/access/Register"
-import Footer from "./components/Footer"
+import Register from "./pages/access/Register";
+import Footer from "./components/Footer";
+import Introduction from "./pages/application-form/Introduction";
 import NotFound from "./pages/NotFound"
 
 function App() {
@@ -19,14 +20,15 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/restorePass" element={<RestorePass />} />
           <Route exact path="/recoverPass" element={<RecoverPass />} />
-          <Route exact path='/register' element={ <Register /> } />
-          <Route exact path='/login' element={ <Login /> } />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/intro" element={<Introduction />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </Router>
     </main>
-  )
+  );
 }
 
 export default App;
