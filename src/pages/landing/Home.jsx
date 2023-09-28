7dc90acd2713e08b591e42284e82fff47fc65318
Mixzio Gonzalez <103139200+MxzGnTh@users.noreply.github.com>
2023-09-28 19:19:00 -0300

    Hu c11 30/m gonzalez (#41)
    * feat:DEVsafiamos realizado(landing)
    
    * Co-authored-by: Gonzalo Fleming <gonzafg2@gmail.com>
    
    * feat:Avance en correccion floating,InitialCOntact e incorporacion de landingForm en inititalContact.
    
    * Floating + InitialContent
    
    * floating button Correccion
    
    * Boton flotante Correcion 1
    
    * Boton flotante modificacion 2

--- src/pages/landing/Home.jsx
+++ src/pages/landing/Home.jsx
@@ -2,15 +2,19 @@
 import ProgramExplanation from "./ProgramExplanation"
 import LandingForm from "../../components/Landing/LandingForm";
 import Banner from "../../components/Landing/Banner";
+import InitialContact from "./InitialContact";
+import FloatingButton from "../../components/FloatingButton";
 const Home = () => {
   return (
     <>
       <div></div>
       <Banner/>
       <ProgramExplanation />
+		<InitialContact/>
       <LandingForm />
+		<FloatingButton/>
     </>
   );
 };
 
-export default Home;
+export default Home
