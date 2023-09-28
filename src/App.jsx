7d13525cdd6502f2d8f635177eb689b3b61d68c9
javiergp23 <javgon23@hotmail.com>
2023-09-27 22:00:48 -0300

    cambios en el componente formulario dos

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import { React } from 'react';
-import HeroSection from './components/Landing/HeroSection.jsx';
-import HeroSectionDos from './components/Landing/HeroSectionDos.jsx';
 import InformacionProfesionalForm from './pages/application-form/InformacionProfesionalForm.jsx';
 
 
 function App() {
   return (
     <>
-    <HeroSection
-    titulo='¿Buscas talento TI?'
-    resumen='Te encontramos profesionales en 5 días y los acompañamos por 3 meses con  tutores senior para potenciar y acelerar sus habilidades técnicas.'/>
-    {/* <HeroSection
-    titulo='¿Buscas Trabajo TI'
-    resumen='Te contactamos con empresas de chile y el extranjero que buscan profesionales TI.'
-    /> */}
-    {/* <InformacionProfesionalForm/> */}
+
+    <InformacionProfesionalForm/> 
+
     </>
   )
 }
